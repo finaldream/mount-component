02685c2bb01f68e4b1d129b5b4cb8bf08f3462a4
olivererdmann <o.erdmann@finaldream.de>
2016-07-05 16:28:42 +0200

    Fixed ES6-syntax in `module.exports`.

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ function mountAll() {
 }
 
 module.exports = {
-    mountComponent,
-    registerComponent,
-    mountAll,
+    mountComponent: mountComponent,
+    registerComponent: registerComponent,
+    mountAll: mountAll,
 };
